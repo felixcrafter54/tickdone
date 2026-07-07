837af6da32e4b2fa09a15d3385189add7d47e9da
Felix Noster <felixcrafter54@gmail.com>
2026-07-08 01:24:13 +0200

    1.0-Vorbereitung: Paketname, App-Icon, GPL-3.0, README
    - Paketname com.example.tickdone -> com.felixcrafter.tickdone (Android
      namespace/applicationId + Kotlin-Pfad, iOS/macOS/Linux Bundle-IDs, Windows
      Runner.rc; Copyright -> Felix Noster).
    - App-Icon aus assets/icon/tickdone.(svg|png) via flutter_launcher_icons fuer
      Android (mipmaps), Web (icons + manifest) und Windows (app_icon.ico) generiert.
    - LICENSE: GNU GPL v3.0 (offizieller Text).
    - README komplett neu (Funktionen, Technik, Bauen, Web-Deploy, Icon, Lizenz).
    
    Version bleibt 1.0.0+1. Hinweis: neue applicationId -> Android installiert als
    neue App (kein Update ueber ein altes com.example-Build).

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (hasKeystore) {
 }
 
 android {
-    namespace = "com.example.tickdone"
+    namespace = "com.felixcrafter.tickdone"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tickdone"
+        applicationId = "com.felixcrafter.tickdone"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
